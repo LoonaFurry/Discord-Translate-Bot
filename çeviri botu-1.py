98bc37527c4faa8fec56c1b11577d6d1ed5907f9
LoonaFurry <127280966+LoonaFurry@users.noreply.github.com>
2023-06-20 05:48:04 +0300

    Update çeviri botu-1.py

--- çeviri botu-1.py
+++ çeviri botu-1.py
@@ -1,15 +1,46 @@
 import discord
 from discord.ext import commands
 from translate import Translator
+from itertools import cycle
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+presences = [
+    "Translating!",
+    "Translating Languages!",
+    "Use Me As Translator!",
+    "I'm Here To Translate!",
+    "Why Don't You Check Translations Skills?",
+    "Looking For Translations!",
+    "Do You Help With Translations?",
+    "I Can Help You With Trasnlations!",
+    "I’m looking for a way to help you understand other languages!",
+    "I’m trying to help with your translation needs!",
+]
+
+
 @bot.event
 async def on_ready():
-    print('Bot is ready.')
+    print(f"{bot.user.name} has connected to Discord!")
+    invite_link = discord.utils.oauth_url(
+        bot.user.id,
+        permissions=discord.Permissions(),
+        scopes=("bot", "applications.commands")
+    )
+    print(f"Invite link: {invite_link}")
+
+    presences_cycle = cycle(presences)
+    while True:
+        presence = next(presences_cycle)
+        presence_with_count = presence.replace("{guild_count}", str(len(bot.guilds)))
+        delay = 30  # Delay in seconds, adjust as needed
+        await bot.change_presence(activity=discord.Game(name=presence_with_count))
+        await asyncio.sleep(delay)
+
 
 @bot.command()
 async def translate(ctx, target_lang: str, *, text: str):
@@ -33,6 +64,6 @@ async def translate(ctx, target_lang: str, *, text: str):
     translated_message = f'Original: {text}\nTranslated ({target_lang}): {translation}'
 
     await ctx.send(translated_message)
-
+    
 # Replace 'YOUR_TOKEN' with your Discord bot token
 bot.run('your-token-here')
